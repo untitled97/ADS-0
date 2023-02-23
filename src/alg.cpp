9a668a977ed4caf0701d080d0b17f84262b05abf
untitled97 <125086986+untitled97@users.noreply.github.com>
2023-02-24 00:44:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,19 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+	if (a <= 0 || b <= 0)
+		std::cout << "Error! You cannot enter negative numbers!" << std::endl;
+	else {
+		while (a%b!=0||b%a!=0)
+		{
+			if (a > b)
+				a -= b;
+			else
+				b -= a;
+		}
+		std::cout << "Your NOD:";
+		if (a % b == 0)	std::cout << a << std::endl;
+		else std::cout << b << std::endl;
+	}
+	return 0;
 }
