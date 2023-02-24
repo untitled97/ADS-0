5dfb8995fc95d1a1df9496c17ab557bab92b335c
untitled97 <125086986+untitled97@users.noreply.github.com>
2023-02-24 22:19:53 +0300

    Update alg.cpp

--- Src/alg.cpp
+++ Src/alg.cpp
@@ -1,9 +1,9 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-if (a <= 0 || b <= 0)
+if (a <= 0 || b <= 0) {
         return 0;
-else {
+} else {
         while (a % b != 0 || b % a != 0) {
                 if (a > b)
                         a -= b;
